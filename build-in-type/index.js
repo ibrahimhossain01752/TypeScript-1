9a08d106ba6737dbe40308d65b504c375f591741
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-11-20 21:15:53 +0600

    build-in-type

---
+++ build-in-type/index.js
@@ -0,0 +1,9 @@
+var userId;
+var firstName;
+var secondName;
+var isActivited;
+userId = 120;
+firstName = "Ibragim";
+secondName = "Hossain";
+isActivited = true;
+console.log(userId);
---
+++ build-in-type/index.ts
@@ -0,0 +1,12 @@
+let userId: number;
+let firstName:string;
+let secondName:string;
+let isActivited:boolean;
+
+
+userId= 120;
+firstName="Ibragim";
+secondName="Hossain";
+isActivited=true;
+
+console.log(userId);
