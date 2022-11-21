95b10e63bfab11a6ed44ae500893d8c3b0244f8c
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-11-21 08:20:47 +0600

    tuple push

--- user-type/tuple-type.js
+++ user-type/tuple-type.js
@@ -1,4 +1,5 @@
 //mixed data
 var userInform;
 userInform = ["Ibrahim", 12];
+userInform.push("Homyra", 13);
 console.log(userInform);
--- user-type/tuple-type.ts
+++ user-type/tuple-type.ts
@@ -2,4 +2,5 @@
 
 let userInform : [string, number];
 userInform = ["Ibrahim",12];
+userInform.push("Homyra",13);
 console.log(userInform);
