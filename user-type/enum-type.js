e43e8fa171bd308391879ceefef5fc8dc40731f9
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2022-11-24 06:43:59 +0600

    enum added

--- user-type/enum-type.js
+++ user-type/enum-type.js
@@ -1,7 +1,22 @@
+//Nummeric Type
 var RequestType;
 (function (RequestType) {
     RequestType[RequestType["readData"] = 0] = "readData";
     RequestType[RequestType["deleteData"] = 1] = "deleteData";
     RequestType[RequestType["saveData"] = 2] = "saveData";
 })(RequestType || (RequestType = {}));
-console.log(RequestType);
+// console.log(RequestType);
+//enum - stringtype
+var RequestType2;
+(function (RequestType2) {
+    RequestType2["readData"] = "Read_Data";
+    RequestType2["deleteData"] = "Delete_Data";
+})(RequestType2 || (RequestType2 = {}));
+// console.log(RequestType2);
+// enum - hetergenous [orthat ekhane number string 2ta niyei kaj kora jabe]
+var RequestType3;
+(function (RequestType3) {
+    RequestType3["readData"] = "Read_Rat";
+    RequestType3[RequestType3["deleteData"] = 101] = "deleteData";
+})(RequestType3 || (RequestType3 = {}));
+console.log(RequestType3['deleteData']);
--- user-type/enum-type.ts
+++ user-type/enum-type.ts
@@ -4,4 +4,20 @@ enum RequestType{
     deleteData, 
     saveData,
 }
-console.log(RequestType);
+// console.log(RequestType);
+
+//enum - stringtype
+
+enum RequestType2{
+    readData = "Read_Data",
+    deleteData = "Delete_Data"
+}
+// console.log(RequestType2);
+
+// enum - hetergenous [orthat ekhane number string 2ta niyei kaj kora jabe]
+
+enum RequestType3{
+    readData = "Read_Rat",
+    deleteData=101
+}
+console.log(RequestType3['deleteData']);
